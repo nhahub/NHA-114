02f491fd544ecbc39e5ca82caaae7a6885cf1053
Youssef2005-MSI <youssefahmed65ah@gmail.com>
2025-11-19 19:27:13 +0200

    Update scores_by_month.sql

--- src/Dbt/scores_by_month.sql
+++ src/Dbt/scores_by_month.sql
@@ -1,1 +1,9 @@
-
+select
+    date_format(Birth_Date, '%Y-%m') as month_bucket,
+    avg((Math + Physics + Chemistry + Biology + English + History) / 6) as avg_score,
+    min((Math + Physics + Chemistry + Biology + English + History) / 6) as min_score,
+    max((Math + Physics + Chemistry + Biology + English + History) / 6) as max_score,
+    count(*) AS student_count
+from {{ ref('Students_SQL') }}
+group by month_bucket
+order by month_bucket
