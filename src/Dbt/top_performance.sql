bd9fd31da64c021a5468284a5c659723149e4288
Youssef2005-MSI <youssefahmed65ah@gmail.com>
2025-11-19 19:28:53 +0200

    Update top_performance.sql

--- src/Dbt/top_performance.sql
+++ src/Dbt/top_performance.sql
@@ -1,1 +1,29 @@
+{{ config(materialized='view') }}
+
+with ranked_scores as (
+    select
+        sc.Student_ID,
+        st.First_Name,
+        st.Last_Name,
+        sb.Subject_Name,
+        sc.Score,
+        row_number() over (
+            partition by sc.Subject_ID
+            order by sc.Score desc
+        ) as rn
+    from {{ ref('scores') }} sc
+    join {{ ref('students') }} st
+        on sc.Student_ID = st.Student_ID
+    join {{ ref('subjects') }} sb
+        on sc.Subject_ID = sb.subject_id
+)
+
+select
+    Student_ID,
+    First_Name,
+    Last_Name,
+    Subject_Name,
+    Score
+from ranked_scores
+where rn = 1
 
