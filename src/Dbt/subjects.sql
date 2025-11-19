a41304b46e595b2c81bb07115c6b733f30923492
Youssef2005-MSI <youssefahmed65ah@gmail.com>
2025-11-19 19:27:47 +0200

    Update subjects.sql

--- src/Dbt/subjects.sql
+++ src/Dbt/subjects.sql
@@ -1,1 +1,14 @@
+-- models/subjects.sql
+
+select 1 as subject_id, 'Math' as subject_name
+union all
+select 2, 'Physics'
+union all
+select 3, 'Chemistry'
+union all
+select 4, 'Biology'
+union all
+select 5, 'English'
+union all
+select 6, 'History'
 
